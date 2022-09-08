f369c24691b4dd6e85eb5ecb4e54b91f35b3a579
Sergey <enchilados@yandex.by>
2022-09-08 09:54:38 +0300

    all solutions

--- C#_Sem5HW_Plotnikov/Program05HW.cs
+++ C#_Sem5HW_Plotnikov/Program05HW.cs
@@ -27,6 +27,7 @@ int counter = 0;
 for (int i = 0; i < array.Length; i++){
     if(array[i]%2==0) counter++;
 }
+// блок вывода результата
 Console.Write("В нём: " + counter + " чётных чисел.");
 }
 // блок ввода размера массива
@@ -49,7 +50,7 @@ CountOdd(array);
 // Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
-
+/*
 int[] MakeMeArray(int size){
 // блок цикла и ввода cлучайных в массив    
     int[] array = new int[size];
@@ -73,6 +74,7 @@ int counter = 0;
 for (int i = 0; i < array.Length; i++){
     if(i%2!=0) counter = counter + array[i];
 }
+// блок вывода результата
 Console.Write("Сумма элементов с нечётным индексом: " + counter +".");
 }
 // блок ввода размера массива
@@ -88,3 +90,51 @@ ShowMyArray(array);
 Console.WriteLine(); // просто отступ
 SumEven(array);
 
+*/
+/*
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+int[] MakeMeArray(int size){
+// блок цикла и ввода cлучайных в массив    
+    int[] array = new int[size];
+    for(int i=0; i<size; i++){
+       array[i] = new Random().Next(-9,99);;
+    }
+    return array;
+    
+} 
+// блок придумывания названия второй функции
+void ShowMyArray(int[] array){
+// блок цикла вывода массива
+    for(int i = 0; i<array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+}
+// блок придумывания названия третьей функции
+void SearchMaxAndMin(int[] array){
+// блок цикла сравнения с сохранением в переменные
+int Max = array[0];
+int Min = array[0];
+int counter = 0;
+for (int i = 0; i < array.Length; i++){
+    if(Max < array[i]) Max = array[i];
+    if(Min > array[i]) Min = array[i];
+}
+    counter = Max - Min;
+// блок вывода результата
+Console.Write($"Разница значений максимального ({Max}) и минимального ({Min}) элементов составляет: " + counter +".");
+}
+// блок ввода размера массива
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(); // просто отступ
+int[] array = MakeMeArray(size);
+
+// блок пояснительной надписи
+Console.Write("Вот ваш сгенерированный массив: ");
+// блок вызова функции вывода массива на экран
+ShowMyArray(array);
+Console.WriteLine(); // просто отступ
+SearchMaxAndMin(array);
+
+*/
