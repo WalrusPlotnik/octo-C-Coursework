4fc906e1f3f249af8ed684bbc50b02bc97f907e6
Sergey <enchilados@yandex.by>
2022-09-09 18:22:28 +0300

    fine

--- C#_Sem6HW_Plotnikov/Program06HW.cs
+++ C#_Sem6HW_Plotnikov/Program06HW.cs
@@ -55,12 +55,12 @@ Console.WriteLine("Введите значение для b2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите значение для k2: "); 
 double k2 = Convert.ToDouble(Console.ReadLine());
-// считаем
+// считаем x, y
 double x = (b2-b1) / (k1-k2);
 double y = k2*x + b2;
 x = Math.Round(x, 2);
 y = Math.Round(y, 2); 
-// показываем
+// показываем x, y
 Console.WriteLine($"При таких данных, точкой пересечения будут координаты X= {x} и Y= {y}.");
 }
 // вызываем функцию
